bd68e8905ed20111dd8a61a8f7d3792d68641901
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2025-07-09 08:43:42 +0200

    Add multi-repo attestation workflow and supporting scripts

---
+++ multi-repo-attestation/1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+# Get the current git commit SHA
+GIT_SHA=$(git rev-parse HEAD)
+ARTIFACT=artifact.txt
+# Write the SHA to artifact.txt
+echo "Git sha is: $GIT_SHA" > $ARTIFACT
+# This script performs the following Kosli operations:
+# 1. Begins a new Kosli trail for the specified Git commit, adding a description with the build number.
+# 2. Attests an artifact to the Kosli trail, specifying artifact type, commit URL, commit hash, artifact file, build URL, and artifact name.
+# 3. Retrieves the Kosli trail data in JSON format and saves it to 'data.json'.
+#
+# Required environment variables:
+# - GIT_COMMIT: The Git commit hash to associate with the Kosli trail.
+# - BUILD_NUMBER: The build number for description purposes.
+# - COMMIT_URL: The URL to the commit in the source control system.
+# - ARTIFACT: The path to the artifact file to attest.
+# - BUILD_URL: The URL to the build/job in the CI system.
+
+kosli begin trail ${GIT_COMMIT} --description "build number ${BUILD_NUMBER}"
+kosli attest artifact --trail=${GIT_COMMIT} --artifact-type=file --commit-url=${COMMIT_URL} --commit=${GIT_COMMIT} $ARTIFACT --build-url=${BUILD_URL} --name=app
+git clone https://github.com/sofusalbertsen/devbox-demo
+cd devbox-demo
+kosli attest generic --name app.sbom --trail ${GIT_COMMIT} --attachments  LICENSE
+kosli attest generic --name app.sbom --trail ${GIT_COMMIT} --commit ${GIT_COMMIT} --attachments  README.md
+
+kosli get trail -o json ${GIT_COMMIT} > data.json
---
+++ multi-repo-attestation/README.md
@@ -0,0 +1,4 @@
+# Multi Repo Attestation
+
+Create a scenario where you clone down a 2nd repo, and performs an attestation in that. What git commit is stored in kosli? And if you provide a --commit, are both then stored?
+
